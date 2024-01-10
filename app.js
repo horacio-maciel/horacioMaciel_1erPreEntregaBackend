b93cef2e919e7e94f8b52b5c2b1b206af8fd867c
horacio maciel <horaciomaciel_91@hotmail.com>
2024-01-10 10:14:12 -0300

    3er desafio entregable

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
-const productManager = require ("./ProductManager")
+const express = require('express')
+const app = express()
 
-const express = require("express")
-const app = express();
+const ProductManager = require("./ProductManager")
 
-app.get("/ping", (req, res) =>{
-    res.send("pong");
-})
+const productManager = new ProductManager("Products.json");
 
+app.get('/products', async (req, res) => {
+    try {
+        const limit = parseInt(req.query.limit) || null;
+        const products = await productManager.getProducts();
 
-app.listen(3000, () => {
-    console.log("aplicacion funcionando en el puerto 3000")
-})
+        if (limit !== null) {
+            res.json({ products: products.slice(0, limit) });
+        } else {
+            res.json({ products });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
+app.get('/products/:pid', async (req, res) => {
+        const product = await productManager.getProductById(
+          parseInt(req.params.pid));
+        
+        if (product) {
+            res.json({ product });
+        } else {
+          res.json({ message: 'el id del producto no fue encontrado' });
+        }
+    
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
